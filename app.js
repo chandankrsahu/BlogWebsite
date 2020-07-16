81efc4d8119fb9588124e7f47e2506f065fe789a
Chandan Kr. Sahu <chandansahuji123@gmail.com>
2020-07-16 23:33:26 +0530

    many features including various routes were added successfully

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+var _ = require('lodash');
 
 app.set('view engine', 'ejs');
 
@@ -19,7 +20,22 @@ app.use(express.static("public"));
 
 
 app.get("/",function(req,res){
-	res.render("home",{some:homeStartingContent});
+	res.render("home",{some:homeStartingContent,posts:posts});
+});
+
+app.get("/posts/:kuch",function(req,res){
+	const x=(req.params.kuch);
+	// var f=0;
+	for(var i=0;i<posts.length;i++)
+	{
+		if(_.lowerCase(x)==_.lowerCase(posts[i].title))
+		{
+			res.render("post",{title:posts[i].title,post:posts[i].post});
+			
+		}	
+	}
+	
+
 });
 
 app.get("/about",function(req,res){
@@ -42,7 +58,8 @@ app.post("/compose",function(req,res){
 	posts.push(x);
 	res.redirect("/");
 	
-})
+});
+
 
 
 
